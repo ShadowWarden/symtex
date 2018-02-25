f648e34dbe9bb66f6d4171a9cdfd56bd7a7c5f88
Omkar Ramachandran <omkar.ramachandran@gmail.com>
2018-02-25 11:00:19 -0700

    Fixed \asin bug

--- symtex.py
+++ symtex.py
@@ -121,7 +121,7 @@ str_input = re.sub(r'(\w)[)] [(](\w)',r'\1[)]*[(]\2',str_input)
 # Get rid of the annoying frac symbols
 str_input = re.sub(r'[\\]frac[\(]','(',str_input)
 
-commands = ['frac','sin','cos','tan','exp','log','Ei','Si','arcsin']
+commands = ['frac','sin','cos','tan','exp','log','Ei','Si','arcsin','asin','acos','atan']
 
 str_input = re.sub(r' ','',str_input)
 
