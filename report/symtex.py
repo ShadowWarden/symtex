5b4a7a462312721d9087137d2d3f8a7c782a00c3
Anthony Tracy <gotbordom@gmail.com>
2018-02-25 12:41:04 -0700

    report update - draft 1.01

--- report/symtex.py
+++ report/symtex.py
@@ -34,14 +34,16 @@ for j in range(i,len(latex_input)-1):
                 k = k+1
             if(count_var == 0):
                 variables += buffer_var
-                variable_dict.append(["\\"+latex_input[j+1:k],buffer_var])
+                variable_dict.append([latex_input[j+1:k],buffer_var])
                 count_var += 1
             else:
                 variables += " "+buffer_var
-                variable_dict.append(["\\"+latex_input[j+1:k],buffer_var])
+                variable_dict.append([latex_input[j+1:k],buffer_var])
                 count_var += 1
+
         elif(latex_input[j+1] == '}' or latex_input[j+1] == '^'):
-            print("Here",latex_input[j],latex_input[j+1],buffer_var)
+            continue
+    #            print("Here",latex_input[j],latex_input[j+1],buffer_var)
         else:
             k = j+1
             while(latex_input[k] != '\\' and latex_input[k] != '}' and latex_input[k] != ' ' and k != len(latex_input)-1):
@@ -78,11 +80,8 @@ while(i < len(latex_input)-1):
         break
     i = i+1
 
-#print(str_command)
 
 # Variable sweep
-#print(variable_dict)
-
 # Use some Regex magic to get the input string to the format we want
 str_input = latex_input[i:-1]
 
@@ -92,7 +91,6 @@ str_input = re.sub(r'[\}]', ')', str_input)
 
 #for i in range(len(variable_dict)):
 #    str_input = re.sub(r'[^]%s' % variable_dict[i][0],'[^]%s' % variable_dict[i][1],str_input)
-#print(str_input)
     
 for i in range(len(variable_dict)):
     str_input = re.sub(r'%s' % variable_dict[i][0],'%s' % variable_dict[i][1],str_input)
@@ -108,10 +106,8 @@ if(len(variable_dict) > 1):
             str_input = re.sub(r'%s%s' % (variable_dict[0][1], variable_dict[1][1]),'%s*%s' % (variable_dict[0][1],variable_dict[1][1]),str_input)
 
 for i in range(len(variable_dict)):
-    str_input = re.sub(r'(\w)%s' % variable_dict[i][1],r'\1*%s' % variable_dict[i][1],str_input)
-    str_input = re.sub(r'%s(\w)' % variable_dict[i][1],r'%s*\1' % variable_dict[i][1],str_input)
-    str_input = re.sub(r'[)]%s' %variable_dict[i][1],r')*%s'% variable_dict[i][1],str_input)
-    str_input = re.sub(r'%s[(]'%variable_dict[i][1] ,r'%s*('% variable_dict[i][1],str_input)
+    str_input = re.sub(r'(\w) %s' % variable_dict[i][1],r'\1*%s' % variable_dict[i][1],str_input)
+    str_input = re.sub(r'%s (\w)' % variable_dict[i][1],r'%s*\1' % variable_dict[i][1],str_input)
     str_input = re.sub(r'[)] %s'%variable_dict[i][1],r')*%s'% variable_dict[i][1],str_input)
     str_input = re.sub(r'%s [(]'%variable_dict[i][1],r'%s*('% variable_dict[i][1],str_input)
 
@@ -121,23 +117,24 @@ str_input = re.sub(r'(\w) (\w)',r'\1*\2',str_input)
 str_input = re.sub(r'(\w)[)] [(](\w)',r'\1[)]*[(]\2',str_input)
 
 # Get rid of the annoying frac symbols
-str_input = re.sub(r'[\\]frac[\(]','(',str_input)
+str_input = re.sub(r'frac[\(]','(',str_input)
 
-commands = ['frac','sin','cos','tan','log','Ei','Si']
+#print(str_input)
+commands = ['abs','frac','sin','cos','tan','exp','log','Ei','Si','arcsin','asin','acos','atan','acot','asec','acsc','sqrt']
 
-for c in commands:
-    str_input = re.sub(r'[\\]%s' % c,'%s' % c,str_input)
+str_input = re.sub(r' ','',str_input)
 
+if(str_input == ""):
+    str_input = "1"
 x = symbols(variables)
 
 #print(str_command)
-#print(str_input)
 #print(variables)
 #print(str(latex(integrate(str_input,x))))
 #print("\""+str_command+"\"","\"intop\"")
 
 if(str_command == "intop" or str_command == "intop " or str_command == "int"):
-    print(latex_input+" = "+str(latex(integrate(str_input,x))))
+    print(latex(integrate(str_input,x)))
 elif(str_command == "diff"):
-    print(latex_input+" = "+latex(diff(str_input,x)))
+    print(latex(diff(str_input,x)))
 
